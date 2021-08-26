152d4c8df1f45ad32bdc41a289491f083c472220
IamVishnuMohan6792 <89527615+IamVishnuMohan6792@users.noreply.github.com>
2021-08-26 09:26:52 +0530

    Update AppVm.tf

--- AppVirtualMachine/AppVm.tf
+++ AppVirtualMachine/AppVm.tf
@@ -24,6 +24,41 @@ resource "azurerm_network_interface" "app-net-interface" {
     }
 }
 
+resource "azurerm_network_security_group" "app-nsg" {
+    name = "app-nsg"
+    location = var.location
+    resource_group_name = var.resource_group
+
+    security_rule {
+        name = "ssh-rule-1"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.1.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "22"
+    }
+    
+    security_rule {
+        name = "ssh-rule-2"
+        priority = 101
+        direction = "Outbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.1.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "22"
+    }
+}
+
+resource "azurerm_subnet_network_security_group_association" "app-nsg-subnet" {
+  subnet_id                 = var.app_subnet_id
+  network_security_group_id = azurerm_network_security_group.app-nsg.id
+}
+
 resource "azurerm_virtual_machine" "app-vm" {
   name = "app-vm"
   location = var.location
